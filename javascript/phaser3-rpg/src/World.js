9ed641a48e92769a2bde9f6edcd983647565c449
Aaron <aaron.pillert@ark.org>
2020-10-16 21:02:13 -0500

    Creating world file to contain base data for each game object

---
+++ javascript/phaser3-rpg/src/World.js
@@ -0,0 +1,11 @@
+export default
+{
+    "Game_Props": {
+        "width": 800,
+        "height": 600,
+        "physics": {
+            "type": "arcade",
+            "gravityY": 300
+        }
+    }
+}
--- javascript/phaser3-rpg/src/main.js
+++ javascript/phaser3-rpg/src/main.js
@@ -1,15 +1,16 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
-import GameScene from './scenes/GameScene'
+import World from './World';
+import GameScene from './scenes/GameScene';
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: World.Game_Props.width,
+	height: World.Game_Props.height,
 	physics: {
-		default: 'arcade',
+		default: World.Game_Props.physics.type,
 		arcade: {
-			gravity: { y: 300 }
+			gravity: { y: World.Game_Props.physics.gravityY }
 		}
 	},
 	scene: [GameScene]
